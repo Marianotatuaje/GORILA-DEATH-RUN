0af240c6a18d5ff0002227c21090706da9d959c8
db032 <db032@lol>
2025-06-15 20:35:05 +0200

    ...

--- GOIRLA DEATH RUN/public_html/javascript1.js
+++ GOIRLA DEATH RUN/public_html/javascript1.js
@@ -1,20 +1,87 @@
-//Escena inicial
-class Escena extends Phaser.Scene {
+// Escena 9: Trampa 1
+class Escena9 extends Phaser.Scene {
+    constructor() {
+        super({ key: 'escena9' });
+    }
 
-    //Càrrega de recursos
     preload() {
-        
-        this.load.image('Escena1', 'imatges/Escena1.png');
-        
-    } 
-    
-     create() {
-        /*
-         * Mitjançant el mètode this.add.sprite col.locam la imatge en pantalla:
-         *  - Primer paràmetre: Valor de la coordenada X on col.locarem el centre de la imatge passada com a tercer paràmetre. En el nostre cas el valor és 480 (960/2).
-         *  - Segon paràmetre: Valor de la coordenada y on col.locarem el centre de la imatge passada com a tercer paràmetre. En el nostre cas el valor és 320 (640/2).
-         *  - Tercer paràmetre: Nom de la imatge que volem col.locar i que hem definit prevaiment en el mètode preload(). En el nostre cas és la imatge "fons".
-         *En el nostre cas, el valor que hem donat a les coordenades és la meitad del valor de l'amplada i alçada de CANVAS, això ens situa l'imatge al centre
-         *de la pantalla.
-         */
-        this.add.sprite(480, 320, 'Escena1');
+        this.load.image('escena9', 'imatges/escena9_trampa1.jpg');
+        this.load.audio('sonido_muerte', 'so/Roblox Death Sound - OOF _ Sound Effect HD - HomeMadeSoundEffects.mp3');
+    }
+
+    create() {
+        this.add.sprite(480, 320, 'escena9');
+        this.sound.play('sonido_muerte');
+    }
+}
+
+// Escena 10: Trampa 2
+class Escena10 extends Phaser.Scene {
+    constructor() {
+        super({ key: 'escena10' });
+    }
+
+    preload() {
+        this.load.image('escena10', 'imatges/escena10_trampa2.jpg');
+        this.load.audio('sonido_muerte', 'so/Roblox Death Sound - OOF _ Sound Effect HD - HomeMadeSoundEffects.mp3');
+    }
+
+    create() {
+        this.add.sprite(480, 320, 'escena10');
+        this.sound.play('sonido_muerte');
+    }
+}
+
+// Escena 11: Banana brillante
+class Escena11 extends Phaser.Scene {
+    constructor() {
+        super({ key: 'escena11' });
+    }
+
+    preload() {
+        this.load.image('escena11', 'imatges/escena11_banana.jpg');
+    }
+
+    create() {
+        this.add.sprite(480, 320, 'escena11');
+        const banana = this.add.zone(420, 250, 120, 140).setOrigin(0).setInteractive();
+        banana.once('pointerdown', () => this.scene.start('escena12'));
+    }
+}
+
+// Escena 12: Final feliç
+class Escena12 extends Phaser.Scene {
+    constructor() {
+        super({ key: 'escena12' });
+    }
+
+    preload() {
+        this.load.image('escena12', 'imatges/escena12_final.jpg');
+        this.load.audio('sonido_victoria', 'so/Victory Sound Effect.mp3');
+    }
+
+    create() {
+        this.add.sprite(480, 320, 'escena12');
+        this.sound.stopAll();
+        this.sound.play('sonido_victoria');
+    }
+}
+
+// Redimensionament
+function resize() {
+    const canvas = document.querySelector("canvas");
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
+    canvas.style.width = windowWidth + 'px';
+    canvas.style.height = windowHeight + 'px';
+}
+
+// Configuració del joc
+const config = {
+    type: Phaser.AUTO,
+    width: 960,
+    height: 640,
+    scene: [Escena, Escena2, Escena3, Escena4, Escena5, Escena6, Escena7, Escena8, Escena9, Escena10, Escena11, Escena12]
+};
+
+new Phaser.Game(config);
